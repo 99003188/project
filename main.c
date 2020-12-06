8b57e5bd3408462909a512d09889d71179b2a74d
99003188 <75420647+99003188@users.noreply.github.com>
2020-12-06 20:32:32 +0530

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 int main()
 {
     int operation;
-    int input1,input2;
+    double input1,input2;
     double height;
     printf("Enter the operation:\n");
     scanf("%d",&operation);
@@ -11,27 +11,27 @@ int main()
     {
     case 1:
         printf("enter the side:\n");
-        scanf("%d",&input1);
-        printf("The area of square = %d",square(input1));
+        scanf("%lf",&input1);
+        printf("The area of square = %lf",square(input1));
         break;
     case 2:
         printf("Enter the length and breadth:\n");
-        scanf("%d%d",&input1,&input2);
+        scanf("%lf%lf",&input1,&input2);
         printf("The area of rectangle = %d",rectangle(input1,input2));
         break;
     case 3:
         printf("Enter the radius and height:\n");
-        scanf("%d%d",&input1,&input2);
+        scanf("%lf%lf",&input1,&input2);
         printf("The volume of a cone = %0.2lf",cone(input1,input2));
         break;
     case 4:
         printf("Enter the radius and height:\n");
-        scanf("%d%d",&input1,&input2);
+        scanf("%lf%lf",&input1,&input2);
         printf("The volume of a cone =%0.2lf",cylinder(input1,input2));
         break;
     case 5:
         printf("Enter the weight and height:\n");
-        scanf("%d%lf",&input1,&height);
+        scanf("%lf%lf",&input1,&height);
         printf("The BMI is %0.2lf",BMI(input1,height));
         break;
     }
